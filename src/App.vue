f52f67328de4bc5b69ae3f76447d793b00bc34f4
daemonlxyut <y.kojima.lab.study@gmail.com>
2021-04-01 01:09:48 +0900

    update application frame by vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,72 @@
 <template>
-  <div id="app" :style="{ backgroundImage: 'url(' + require('@/assets/bg.png') + ')' }">
-    <tab-menu/>
-    <router-view/>
-  </div>
+<div id="app">
+  <v-app id="inspire">
+    <!-- Title Bar -->
+    <v-app-bar app color="#350800" dark clipped-left>
+      <v-spacer/>
+      <v-img contain max-height="60%" :src="require('@/assets/logo/nAIdemoc_white.png')" />
+      <v-spacer/>
+    </v-app-bar>
+
+    <!-- Navigation Menu -->
+    <v-navigation-drawer app clipped v-model="drawer" :mini-variant="mini" permanent>
+      <v-list-item class="px-2">
+        <v-btn icon @click="mini=!mini">
+          <v-icon v-if="mini">mdi-menu</v-icon>
+          <v-icon v-else>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.name" link :to="item.path">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Main Contents -->
+    <v-main>
+      <router-view/>
+    </v-main>
+
+    <!-- Footer -->
+    <v-footer app color="#350800" class="white--text">
+      <v-spacer/>
+      <span>&copy; 2021 {{ team }}</span>
+      <v-spacer/>
+    </v-footer>
+  </v-app>
+</div>
 </template>
 
 <script>
-import TabMenu from '@/components/TabMenu'
-
 export default {
   name: 'App',
-  components: {
-    TabMenu
+  props: {
+    source: String,
+  },
+  data () {
+    return {
+      drawer: true,
+      mini: true,
+      items: [
+        {
+          name: "Home",
+          path: "/",
+          icon: "mdi-home",
+        },
+        {
+          name: "Chat",
+          path: "/chat",
+          icon: "mdi-robot-excited",
+        },
+      ],
+      team: "nonodebris.com"
+    }    
   }
 }
 </script>
-
-<style>
-* {
-  /* 枠幅の設定 */
-  padding: 0;
-  margin: 0;
-}
-
-#app {
-  /* フォント系設定 */
-  font-family: "游ゴシック体", YuGothic, "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック", "Yu Gothic", sans-serif;
-  font-weight: 500;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #31302f;
-
-  min-height: 100vh;
-  background-attachment: fixed;
-  background-size: cover;
-  background-position: center;
-}
-</style>
